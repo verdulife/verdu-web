b7726c9f9b69dfe9041f2f92e00cb69e125263a7
verdulife <verdu@live.com>
2022-02-24 14:02:43 +0100

    fix with adapter-vercel

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-#testing typescript
+My personal portfolio
+===
+
+```Comming soon everything you need to know about me. 😀```
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from "@sveltejs/adapter-vercel";
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 import { resolve } from 'path';
@@ -15,7 +15,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 		vite: {
 			resolve: {
 				alias: {
