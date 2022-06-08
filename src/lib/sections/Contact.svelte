9dab66a5fc5707a041cb58f7d839a576d7376bed
verdulife <verdu@live.com>
2022-06-08 12:20:44 +0200

    til projects redesign 3

--- src/lib/sections/Contact.svelte
+++ src/lib/sections/Contact.svelte
@@ -4,10 +4,10 @@
 
 <section id="contact" class="col acenter xfill">
 	<h2>
-		If you want to contact me, you can use this
-		<a href="https://www.linkedin.com/in/albert-verd%C3%BA-llinares-25ba19134/" target="_blank">
-			<strong>linkedin link</strong>
-		</a>, but if you feel oldschool, you can fill this form.
+		If you want to contact me, you can use this <a
+			href="https://www.linkedin.com/in/albert-verd%C3%BA-llinares-25ba19134/"
+			target="_blank"><strong>linkedin link</strong></a
+		>, but if you feel oldschool, you can fill this form.
 	</h2>
 
 	<Form />
